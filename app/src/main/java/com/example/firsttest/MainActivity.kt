03781ef8d5280ecba408caf284e9da8c049afd5e
Johanna Pehkonen <johanna.pehkonen@student.oulu.fi>
2024-01-12 12:59:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firsttest/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.firsttest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firsttest.ui.theme.FirstTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "message"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.kissa),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+            )
+        Spacer(modifier = Modifier.width(8.dp))
+    }
+
+
+    Column {
+        Text(text = msg.author)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = msg.body)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(
+        msg = Message("Lexi", "Hey, this is a message!")
+    )
+}
+
+
+
