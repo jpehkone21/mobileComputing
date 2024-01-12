98f12d0143bf8c3fccf05838a227f190d74e9d3b
Johanna Pehkonen <johanna.pehkonen@student.oulu.fi>
2024-01-12 13:45:37 +0200

    task continued

--- app/src/main/java/com/example/firsttest/MainActivity.kt
+++ app/src/main/java/com/example/firsttest/MainActivity.kt
@@ -6,13 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firsttest.ui.theme.FirstTestTheme
 
@@ -29,22 +37,22 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.kissa),
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-            )
+        )
         Spacer(modifier = Modifier.width(8.dp))
-    }
 
 
-    Column {
-        Text(text = msg.author)
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.body)
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
     }
 }
 
