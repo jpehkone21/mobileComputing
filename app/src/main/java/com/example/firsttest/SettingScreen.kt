9a2239caa640c25595489fd3ca11c85d90c830e4
jpehkone21 <102893767+jpehkone21@users.noreply.github.com>
2024-01-19 19:46:02 +0200

    warnings removed

--- app/src/main/java/com/example/firsttest/SettingScreen.kt
+++ app/src/main/java/com/example/firsttest/SettingScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
 
 @Composable
